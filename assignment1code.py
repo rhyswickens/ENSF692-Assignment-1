1c7ff78075ac844db67f659a131926b74013244c
rhyswickens <168765053+rhyswickens@users.noreply.github.com>
2024-05-09 09:01:00 -0600

    Add files via upload

---
+++ assignment1code.py
@@ -0,0 +1,11 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Author: Rhys Wickens
+
+studentName = "Rhys Wickens"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+print(studentName + "' best skill is hockey.")
